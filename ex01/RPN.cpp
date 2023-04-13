c2597bcf43c486dc2bee67968212b1879da3eb31
Robin BONY <rbony@student.42lyon.fr>
2023-04-13 09:19:23 +0200

    rpn ok

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,131 +6,98 @@
 /*   By: rbony <rbony@corobizar.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 12:25:47 by rbony             #+#    #+#             */
-/*   Updated: 2023/03/19 16:50:40 by rbony            ###   ########lyon.fr   */
+/*   Updated: 2023/04/12 12:57:29 by rbony            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
-bool RPN::isOperator(const char c)
+bool RPN::isoperator(const char c)
 {
-  if (c == '+')
-    return true;
-  if (c == '-')
-    return true;
-  if (c == '*')
-    return true;
-  if (c == '/')
-    return true;
-  return false;
-}
-
-bool RPN::checkExpression(const std::string &expression)
-{
-  const char *str = removeWhitespaces(expression).c_str();
-  long operatorCount = 0;
-  long digitCount = 0;
-
-  for (int i = 0; str[i]; i++)
-  {
-    if (isdigit(str[i]))
-      digitCount++;
-    else if (isOperator(str[i]))
-      operatorCount++;
-    else
-      return false;
-  }
-  if (operatorCount == (digitCount - 1))
-    return true;
-  return false;
-}
-
-RPN::RPN(std::string expression)
-{
-  if (!checkExpression(expression.c_str()))
-  {
-    OperationException ex("Error");
-    throw ex;
-  }
-  std::string token;
-  std::stringstream ss(expression);
-  
-  while (std::getline(ss, token, ' ')) {
-    if (checkToken(token)) 
-    {
-        int digit = atoi(token.c_str());
-        if (digit < 10)
-            this->tokens.push(digit);
-        else
-        {
-          if (digit < 100)
-          {
-            this->tokens.push(digit / 10);
-            this->tokens.push(digit % 10);
-          }
-          else
-          {
-            OperationException ex("Error");
-            throw ex;
-          }
-        }
-    } 
-    else if(token == "+" || token == "-" || token == "*" || token == "/")
-    {
-      if (this->tokens.size() == 2)
-      {
-        int operand2 = this->tokens.top();
-        this->tokens.pop();
-        int operand1 = this->tokens.top();
-        this->tokens.pop();
-
-        if (token == "+") {
-          this->tokens.push(operand1 + operand2);
-        } else if (token == "-") {
-          this->tokens.push(operand1 - operand2);
-        } else if (token == "*") {
-          this->tokens.push(operand1 * operand2);
-        } else if (token == "/") {
-          this->tokens.push(operand1 / operand2);
-        }
-      }
-      else
-      {
-        OperationException ex("Error");
-        throw ex;
-      }
-    }
-    else
-    {
-        OperationException ex("Error");
-        throw ex;
-    }
-  }
-  std::cout << this->tokens.top() << std::endl;
-}
-
-RPN::~RPN()
-{
-    
+	if (c == '+')
+		return true;
+	if (c == '-')
+		return true;
+	if (c == '*')
+		return true;
+	if (c == '/')
+		return true;
+	return false;
 }
 
 std::string RPN::removeWhitespaces(const std::string &str) const
 {
-    std::string newStr = "";
-    for (std::string::const_iterator it = str.begin(); it != str.end(); it++)
-    {
-        if(!std::isspace(*it))
-            newStr += *it;
-    }
-    return newStr;
+	std::string newStr = "";
+	for (std::string::const_iterator it = str.begin(); it != str.end(); it++)
+	{
+		if (!std::isspace(*it))
+			newStr += *it;
+	}
+	return newStr;
 }
 
-bool RPN::checkToken(const std::string &str) const
+RPN::RPN(std::string expression)
 {
-  for (size_t i = 0; i < str.size(); i++)
-  {
-    if (!isdigit(str[i]))
-      return false;
-  }
-  return true;  
+	const char *str = removeWhitespaces(expression).c_str();
+
+	for (size_t i = 0; i < expression.length(); i++)
+	{
+		if (isdigit(str[i]))
+		{
+			int digit = str[i] - 48;
+			this->tokens.push(digit);
+		}
+		else if (isoperator(str[i]))
+		{
+			if (this->tokens.size() > 1)
+			{
+				int operand2 = this->tokens.top();
+				this->tokens.pop();
+				int operand1 = this->tokens.top();
+				this->tokens.pop();
+
+				if (str[i] == '+')
+				{
+					this->tokens.push(operand1 + operand2);
+				}
+				else if (str[i] == '-')
+				{
+					this->tokens.push(operand1 - operand2);
+				}
+				else if (str[i] == '*')
+				{
+					this->tokens.push(operand1 * operand2);
+				}
+				else if (str[i] == '/')
+				{
+					if (operand2 == 0)
+					{
+						OperationException ex("Error");
+						throw ex;
+					}
+					this->tokens.push(operand1 / operand2);
+				}
+			}
+			else
+			{
+				OperationException ex("Error");
+				throw ex;
+			}
+		}
+		else
+		{
+			OperationException ex("Error");
+			throw ex;
+		}
+	}
+	if (this->tokens.size() > 1)
+	{
+		OperationException ex("Error");
+		throw ex;
+	}
+	std::cout << this->tokens.top() << std::endl;
+}
+
+RPN::~RPN()
+{
 }
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,53 +6,50 @@
 /*   By: rbony <rbony@corobizar.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 12:26:02 by rbony             #+#    #+#             */
-/*   Updated: 2023/03/19 16:48:57 by rbony            ###   ########lyon.fr   */
+/*   Updated: 2023/04/12 12:51:40 by rbony            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <stack>
 #include <string>
-#include <sstream>
 #include <cstdlib>
 
 #ifndef RPN_HPP
 #define RPN_HPP
 
-
 class RPN
 {
-    private:
-        std::stack<long> tokens;
+private:
+	std::stack<long> tokens;
 
-        bool checkToken(const std::string &str) const;
-        bool isOperator(const char c);
-        bool checkExpression(const std::string &expression);
-        std::string removeWhitespaces(const std::string &str) const;
+	bool isoperator(const char c);
+	std::string removeWhitespaces(const std::string &str) const;
 
-        RPN(const RPN &f);
-        RPN &operator=(const RPN &f);
-        RPN();
+	RPN(const RPN &f);
+	RPN &operator=(const RPN &f);
+	RPN();
 
-    public:
-        RPN(std::string expression);
-        ~RPN();
-    
-    class OperationException : public std::exception
-    {
-        private:
-            const std::string message;
-        public:
-            OperationException(std::string message): message(message){};
-            virtual ~OperationException() throw(){};
+public:
+	RPN(std::string expression);
+	~RPN();
 
-            virtual const char *what() const throw()
-            {
-            return this->message.c_str();
-           }
-    };
+	class OperationException : public std::exception
+	{
+	private:
+		const std::string message;
+
+	public:
+		OperationException(std::string message) : message(message){};
+		virtual ~OperationException() throw(){};
+
+		virtual const char *what() const throw()
+		{
+			return this->message.c_str();
+		}
+	};
 };
 
-std::ostream& operator<<(std::ostream& os, const RPN& rpn);
+std::ostream &operator<<(std::ostream &os, const RPN &rpn);
 
 #endif
